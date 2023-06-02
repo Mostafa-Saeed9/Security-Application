70ebf25fd491c8f6a512fd74b189c36b620cfd48
Mostafa Saeed <mostafa.saeedx98@gmail.com>
2023-06-02 21:46:34 +0300

    uploaded

---
+++ Security_App/src/main/java/com/practice/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.practice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practice.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findByUsernameOrEmail(String username, String email);
+
+	Optional<User> findByUsername(String username);
+
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
+
+}
